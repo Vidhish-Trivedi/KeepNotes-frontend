6083087f61a6951745c45d5770c012bb52def62f
Vidhish Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2023-03-11 09:46:50 +0530

    Initial project setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//4. Create a Footer.jsx component that renders a <footer> element
+//to show a copyright message in a <p> with a dynamically updated year.
+//6. Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
