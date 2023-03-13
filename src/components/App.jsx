27e6f7148ae8718ef40ec09711deab8909d5bd84
Vidhish Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2023-03-13 18:34:14 +0530

    Render multiple cards for Note component using map

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App(){
     return(
         <div>
             <Header />
-            <Note />
-            <Note />
-            <Note />
+            {notes.map((note) => {
+                return(
+                    <Note key={note.id} title={note.title} content={note.content}/>
+                );
+            })}
             <Footer />
         </div>
     );
